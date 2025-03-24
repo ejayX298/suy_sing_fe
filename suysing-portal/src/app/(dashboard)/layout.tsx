15c8e5deb195ef25ed8571d491324c2671f4fa3d
Brian Prado <67305006+brianblip@users.noreply.github.com>
2025-03-24 17:32:42 +0800

    add dashboard page

--- suysing-portal/src/app/(dashboard)/layout.tsx
+++ suysing-portal/src/app/(dashboard)/layout.tsx
@@ -50,11 +50,11 @@ export default function DashboardLayout({
   }
 
   return (
-    <div className="flex h-screen ">
+    <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Header title={getPageTitle()} />
-        <main className="flex-1 overflow-x-hidden overflow-y-auto  p-6">
+        <main className="flex-1 overflow-x-hidden overflow-y-auto px-6 py-10">
           {children}
         </main>
       </div>
