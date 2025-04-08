4246802f1ca757fcccd013c60467a96a9ee2bd59
EJ Von Taasan <ejvon@clearmindai.com>
2025-04-08 19:32:36 +0800

    update BootItem component

--- suysing-app/src/components/BoothItem.tsx
+++ suysing-app/src/components/BoothItem.tsx
@@ -11,15 +11,15 @@ export default function BoothItem({ name, logo }: BoothItemProps) {
   const imageSrc = logo.startsWith('/') 
     ? logo
     : hasExtension 
-      ? `/images/main-hall/${logo}`
-      : `/images/main-hall/${logo}.png`;
+      ? `/images/booths/${logo}`
+      : `/images/booths/${logo}.png`;
 
   return (
     <div className="flex items-center p-3 border-b border-[#7D7D7D] bg-white last:border-b-0">
       <div className="w-24 h-8 relative mr-4 shrink-0">
         <Image
           src={imageSrc}
-          alt={`${name} logo`}
+          alt={`${logo} logo`}
           fill
           objectFit="contain"
           unoptimized
