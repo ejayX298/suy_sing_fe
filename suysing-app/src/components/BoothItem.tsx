9b0404307f7f54a69b769b1cd615658cb53a8725
Brian Prado <67305006+brianblip@users.noreply.github.com>
2025-04-08 20:00:29 +0800

    refactorBoothItem page

--- suysing-app/src/components/BoothItem.tsx
+++ suysing-app/src/components/BoothItem.tsx
@@ -2,28 +2,14 @@ import Image from "next/image";
 
 interface BoothItemProps {
   name: string;
-  logo: string;
+  image: string;
+  boothCode: string;
 }
-
-export default function BoothItem({ name, logo }: BoothItemProps) {
-  const hasExtension = /\.(png|jpg|jpeg|gif|webp)$/i.test(logo);
-  
-  const imageSrc = logo.startsWith('/') 
-    ? logo
-    : hasExtension 
-      ? `/images/main-hall/${logo}`
-      : `/images/main-hall/${logo}.png`;
-
+export default function BoothItem({ name, image, boothCode }: BoothItemProps) {
   return (
     <div className="flex items-center p-3 border-b border-[#7D7D7D] bg-white last:border-b-0">
-      <div className="w-24 h-8 relative mr-4 shrink-0">
-        <Image
-          src={imageSrc}
-          alt={`${name} logo`}
-          fill
-          objectFit="contain"
-          unoptimized
-        />
+      <div className="w-18 h-12 relative mr-4 shrink-0">
+        <Image src={image} alt={boothCode} fill  />
       </div>
       <span className="text-gray-800 font-medium">{name}</span>
     </div>
