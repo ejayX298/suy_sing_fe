06c1668b6d29a50a5a6c1f9f27a09183b4d08fdb
EJ Von Taasan <ejvon@clearmindai.com>
2025-04-11 00:34:24 +0800

    revert to baseUrl to env variable

--- suysing-app/src/services/base/httpClient.ts
+++ suysing-app/src/services/base/httpClient.ts
@@ -3,8 +3,7 @@ import axios from 'axios';
 export default function httpClient(api_key : string) {
   
   const basehttpClient = axios.create({
-    // baseURL: process.env.NEXT_PUBLIC_API_URL,
-    baseURL : 'https://suysing-app.escarez.com/api',
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
       'Content-Type': 'multipart/form-data',
       Accept: 'application/json',
