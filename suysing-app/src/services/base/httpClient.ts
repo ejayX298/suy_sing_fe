4e7c67c61f0acb91d1f1a65bc7379555fbbc33a3
EJ Von Taasan <ejvon@clearmindai.com>
2025-04-11 00:16:29 +0800

    update baseURL

--- suysing-app/src/services/base/httpClient.ts
+++ suysing-app/src/services/base/httpClient.ts
@@ -3,12 +3,12 @@ import axios from 'axios';
 export default function httpClient(api_key : string) {
   
   const basehttpClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    // baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL : 'https://suysing-app.escarez.com/api',
     headers: {
       'Content-Type': 'multipart/form-data',
       Accept: 'application/json',
-      // 'X-API-KEY':`${api_key}`,
-      'Authorization':`Bearer ${api_key}`,
+      'X-API-KEY':`${api_key}`
     },
   });
 
