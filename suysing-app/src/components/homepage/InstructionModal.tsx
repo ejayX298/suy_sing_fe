05eff86ddd9a827157276dde0f0eee021eb0e911
EJ Von Taasan <ejvon@clearmindai.com>
2025-04-13 20:26:46 +0800

    added logic for displaying of modal

--- suysing-app/src/components/homepage/InstructionModal.tsx
+++ suysing-app/src/components/homepage/InstructionModal.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WelcomePage from "./WelcomePage";
 import CameraInstructionPage from "./CameraInstructionPage";
 import DealFormInstructionPage from "./DealFormInstructionPage";
 import BestBoothInstructionPage from "./BestBoothInstructionPage";
 
 interface InstructionModalProps {
+  customer_data : any;
   isOpen: boolean;
   onClose: () => void;
 }
 
-export default function InstructionModal({ isOpen, onClose }: InstructionModalProps) {
+export default function InstructionModal({ customer_data, isOpen, onClose }: InstructionModalProps) {
   const [currentStep, setCurrentStep] = useState<'welcome' | 'camera' | 'deal-form' | 'best-booth'>('welcome');
 
   if (!isOpen) return null;
@@ -29,14 +30,33 @@ export default function InstructionModal({ isOpen, onClose }: InstructionModalPr
   };
 
   const handleClose = () => {
+    handleFirstVisitIds(customer_data?.id || 0);
     setCurrentStep('welcome');
     onClose();
   };
 
+  const handleFirstVisitIds = (customer_id : number) =>{
+    const firstVisitIds = localStorage.getItem('firstVisitIds');
+    console.log(customer_id)
+    if(customer_id && customer_id != 0){  
+      if(!firstVisitIds){
+        const firstVisitIdsStore = [customer_id]
+        localStorage.setItem('firstVisitIds', JSON.stringify(firstVisitIdsStore));
+      }else{
+        const currentFirstVisitIds = JSON.parse(firstVisitIds);
+        // Insert id in local storage if not exist
+        if(!currentFirstVisitIds.includes(customer_id)){
+            const updatedFirstVisitedIdsStore = [...currentFirstVisitIds, customer_id]
+            localStorage.setItem('firstVisitIds', JSON.stringify(updatedFirstVisitedIdsStore));
+        }
+      }
+    }
+  }
+
   return (
     <>
       {currentStep === 'welcome' && (
-        <WelcomePage onContinue={handleNext} onClose={handleClose} />
+        <WelcomePage customer_data={customer_data} onContinue={handleNext} onClose={handleClose} />
       )}
       {currentStep === 'camera' && (
         <CameraInstructionPage onClose={handleNext} />
