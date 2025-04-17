37e1476120bf6e9eb5c6f96c5823a4de162205d6
Brian Prado <67305006+brianblip@users.noreply.github.com>
2025-04-18 07:49:45 +0800

    fix booths sizes

--- suysing-app/src/components/BoothItem.tsx
+++ suysing-app/src/components/BoothItem.tsx
@@ -6,11 +6,21 @@ interface BoothItemProps {
   boothCode?: string;
 }
 
-export default function BoothItem({ name, image = "", boothCode = "" }: BoothItemProps) {
+export default function BoothItem({
+  name,
+  image = "",
+  boothCode = "",
+}: BoothItemProps) {
   return (
     <div className="flex items-center p-3 border-b border-[#7D7D7D] bg-white last:border-b-0">
-      <div className="w-18 h-12 relative mr-4 shrink-0">
-        <Image src={image} alt={boothCode} fill  />
+      <div className="relative mr-4 shrink-0">
+        <Image
+          src={image}
+          alt={boothCode}
+          width={72}
+          height={48}
+          className="object-contain"
+        />
       </div>
       <span className="font-medium">{name}</span>
     </div>
