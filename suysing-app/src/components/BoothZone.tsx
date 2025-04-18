0d86b3a8d092732a3f456ecaed6194911a7be932
Brian Prado <67305006+brianblip@users.noreply.github.com>
2025-04-18 17:17:36 +0800

    remove sorting

--- suysing-app/src/components/BoothZone.tsx
+++ suysing-app/src/components/BoothZone.tsx
@@ -29,7 +29,6 @@ export default function BoothZone({ title, booths, progress }: BoothZoneProps) {
           </div>
           {booths
             .filter((booth) => !booth.visited)
-            .sort((a, b) => a.name.localeCompare(b.name))
             .map((booth) => (
               <BoothItem
                 key={booth.name}
