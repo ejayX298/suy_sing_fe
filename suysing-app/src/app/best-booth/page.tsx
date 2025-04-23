6407ef151dce44c7c1e98683b2968a935fe013a1
Brian Prado <67305006+brianblip@users.noreply.github.com>
2025-04-23 13:46:32 +0800

    add margin bottom

--- suysing-app/src/app/best-booth/page.tsx
+++ suysing-app/src/app/best-booth/page.tsx
@@ -376,7 +376,7 @@ function BestBoothContent() {
   };
 
   return (
-    <div className="flex flex-col min-h-screen ">
+    <div className="flex flex-col min-h-screen mb-6">
       {isRender && isDoneVisit && (
         <div className="flex-1 pb-16">
           {showVotedMessage ? (
