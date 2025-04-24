2a4a7908bac351dea36d5afa55b563f7d32c8855
Brian Prado <67305006+brianblip@users.noreply.github.com>
2025-04-24 22:11:51 +0800

    update message

--- suysing-app/src/app/best-booth/page.tsx
+++ suysing-app/src/app/best-booth/page.tsx
@@ -385,14 +385,17 @@ function BestBoothContent() {
                 <div className="flex flex-col items-center text-center">
                   <h2 className="text-xl font-bold mb-4">Thank You!</h2>
                   <p className="text-gray-600 mb-6">
-                    You&apos;re done voting! You can now claim your souvenir.
-                    Thank you.
+                    <span className="text-lg">
+                      You&apos;ve already submitted your Best Booth votes.
+                    </span>
+                    Claim your Suki Day Souvenir at the Tent Lobby from 2:30pm
+                    until 7:00pm.
                   </p>
                   <button
                     onClick={() => router.push(`/?cc=${stored_hash_code}`)}
                     className="w-full py-3 bg-[#F78B1E] hover:bg-orange-600 text-black font-semibold rounded-md"
                   >
-                    Go to Booth Map
+                    Proceed
                   </button>
                 </div>
               </div>
