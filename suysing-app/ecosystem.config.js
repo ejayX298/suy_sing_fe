36849ec6fc18f2528f95f5ce77cfe3aef87d1bf4
rai <rai@escarez.com>
2025-04-27 06:30:07 +0000

    working prod config

--- suysing-app/ecosystem.config.js
+++ suysing-app/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "app",
     script: "npm",
     args: "run build-and-start",
-    watch: ["package.json","next.config.ts","tsconfig.json","eslint.config.mjs","postcss.config.mjs", "src", "public"],
+    watch: [".env","ecosystem.config.js","package.json","next.config.ts","tsconfig.json","eslint.config.mjs","postcss.config.mjs", "src", "public"],
     ignore_watch: ["node_modules", ".next"]
   }]
 }
