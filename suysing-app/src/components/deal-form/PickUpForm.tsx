e24a37a3970574b6f87c7ea48783ac3551b7af4d
EJ Von Taasan <ejvon@clearmindai.com>
2025-05-02 13:09:20 +0800

    update initial deal form page

--- suysing-app/src/components/deal-form/PickUpForm.tsx
+++ suysing-app/src/components/deal-form/PickUpForm.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 interface PickUpFormProps {
   customerCode: string;
   customerSubCode: string;
+  customerSubCodeId : string;
   email: string;
   transactionType: string;
   branch: string;
@@ -14,6 +15,8 @@ interface PickUpFormProps {
   ) => void;
   onSelectChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
   onNext: () => void;
+  subCodes: SubCode[];
+  onCustomerCodeChange?: (e: React.ChangeEvent<HTMLSelectElement>)  => void;
 }
 
 interface CustomerPickupDetails {
@@ -22,9 +25,18 @@ interface CustomerPickupDetails {
   branch_code: string;
 }
 
+interface SubCode {
+  id: number;
+  code: string;
+  transaction_type: string;
+  ship_to: string | null;
+  payment_code: string;
+}
+
 export default function PickUpForm({
   customerCode,
   customerSubCode,
+  customerSubCodeId,
   email,
   transactionType,
   branch,
@@ -34,21 +46,22 @@ export default function PickUpForm({
   onNext,
   transactionTypes,
   customerPickupDetails,
+  subCodes = [],
+  onCustomerCodeChange,
 }: PickUpFormProps) {
-  const [showInfo, setShowInfo] = useState(false);
+  // const [showInfo, setShowInfo] = useState(false);
   return (
     <div className="bg-white rounded-md border-2 border-gray-400 shadow-sm overflow-hidden max-w-2xl mx-auto">
       <div className="px-6 mt-6">
         <h2 className="text-2xl font-bold text-black text-left">Deal Form</h2>
       </div>
       <div className="px-6 space-y-5 py-4 font-light">
-        <div>
-          <label
+          {/* <label
             htmlFor="customerCode"
             className="block text-sm  text-black mb-2"
           >
             Customer Code
-          </label>
+          </label> */}
           <input
             type="hidden"
             id="customerCode"
@@ -57,7 +70,7 @@ export default function PickUpForm({
             className="w-full px-3  py-4 border border-gray-400 rounded-md focus:outline-none text-black"
             readOnly
           />
-          <input
+          {/* <input
             type="text"
             id="customerSubCode"
             name="customerSubCode"
@@ -65,9 +78,58 @@ export default function PickUpForm({
             className="w-full px-3  py-4 border border-gray-400 rounded-md focus:outline-none text-black"
             readOnly
           />
+         */}
+
+        <div>
+          <label
+            htmlFor="customerSubCodeId"
+            className="block text-sm  text-black mb-2"
+          >
+            Customer Code
+          </label>
+          {subCodes.length > 0 ?(
+
+              <div className="relative">
+                <select
+                  id="customerSubCodeId"
+                  name="customerSubCodeId"
+                  value={customerSubCodeId}
+                  onChange={onCustomerCodeChange}
+                  className="w-full px-3 py-4 border border-gray-400 rounded-md text-black focus:outline-none appearance-none"
+                >
+                  {subCodes.map((subCodeOption, index) => (
+                    <option key={index} value={subCodeOption.id}>
+                      {subCodeOption.code}
+                    </option>
+                  ))}
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-gray-700">
+                  <svg
+                    className="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                  </svg>
+                </div>
+              </div>
+
+          ) : (
+
+              <input
+                type="text"
+                id="customerSubCode"
+                name="customerSubCode"
+                value={customerSubCode}
+                className="w-full px-3  py-4 border border-gray-400 rounded-md focus:outline-none text-black"
+                readOnly
+              />
+
+          )}
+
         </div>
 
-        <div className="relative">
+        {/* <div className="relative">
           <div className="flex items-center gap-1">
             <label htmlFor="email" className="block text-sm  text-black mb-2">
               Email Address (optional)
@@ -94,7 +156,7 @@ export default function PickUpForm({
               </p>
             </div>
           )}
-        </div>
+        </div> */}
 
         <div>
           <label
